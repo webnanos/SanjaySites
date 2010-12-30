10f3168cb793fc9fdf4e0de552edb02bf6fa4a38
Webnanos <webnanos.corp@gmail.com>
2010-12-30 23:13:46 +0530

    Deployment Script

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+set :application, "SanjaySites"
+default_run_options[:pty] = true  # Must be set for the password prompt from git to work
+set :repository,  "git@github.com:webnanos/SanjaySites.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :user, "webnanos"  # The server's user for deploys
+set :scm_passphrase, "suraag_1"  # The deploy user's password
+
+set :branch, "master"
+
+set :deploy_via, :remote_cache
+
+
+
+role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+role :app, "your app-server here"                          # This may be the same as your `Web` server
+role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+role :db,  "your slave db-server here"
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# namespace :deploy do
+#   task :start {}
+#   task :stop {}
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
